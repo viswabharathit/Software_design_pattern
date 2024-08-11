a8abdbd6b548b6d8e2f8e0475d9113743cc2e92f
VISWABHARATHI T <125241243+viswabharathit@users.noreply.github.com>
2024-08-11 09:33:21 +0530

    Add files via upload

---
+++ DAY7/src/components/pages/WordPullUpDemo.jsx
@@ -0,0 +1,11 @@
+import WordPullUp from "@/components/magicui/word-pull-up";
+
+const WordPullUpDemo=()=> {
+  return (
+    <WordPullUp
+      className="text-4xl font-bold tracking-[-0.02em] text-black dark:text-white md:text-7xl md:leading-[5rem]"
+      words="Word Pull Up"
+    />
+  );
+}
+export default WordPullUpDemo;
